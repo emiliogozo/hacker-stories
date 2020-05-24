5f88d5f46bbb69f15830068a9ea56e56f80f8d36
emiliogozo <emilio.gozo@obf.ateneo.edu>
2020-05-24 21:53:14 +0800

    explicit data fetching

--- src/App.js
+++ src/App.js
@@ -78,25 +78,27 @@ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
+
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false });
   
-    const handleFetchStories = React.useCallback(() => {
-      if (!searchTerm) return;
-      
-      dispatchStories({ type: 'STORIES_FETCH_INIT' });
-  
-      fetch(`${API_ENDPOINT}${searchTerm}`)
-        .then(response => response.json())
-        .then(result => {
-          dispatchStories({
-            type: 'STORIES_FETCH_SUCCESS',
-            payload: result.hits
-          });
-        })
-        .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-    }, [searchTerm]);
+  const handleFetchStories = React.useCallback(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits
+        });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -109,8 +111,12 @@ const App = () => {
     });
   };
 
-  const handleSearch = event => {
+  const handleSearchInput = event => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   // const searchedStories = stories.data.filter(story => 
@@ -125,11 +131,19 @@ const App = () => {
         id="search"
         label="Search"
         value={searchTerm}
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
         isFocused
       >
         <strong>Search:</strong>
       </InputWithLabel>
+
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
         
       <hr />
 
